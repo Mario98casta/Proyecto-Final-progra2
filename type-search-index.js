8e48ff7938b07b5ef1f40996dc8127eeb9b75853
Mario98casta <109645043+Mario98casta@users.noreply.github.com>
2022-10-27 22:11:17 -0600

    Add files via upload

---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"model","l":"Comentarios"},{"p":"<Unnamed>","l":"InsertarNoticiasjdbc"},{"p":"dao","l":"ListNoticias"},{"p":"<Unnamed>","l":"main"},{"p":"model","l":"Noticias"},{"p":"<Unnamed>","l":"ObtenerNoticias"}];updateSearchResults();
